5d89b01dd779e249d68cba84696f007160e7b745
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-04-22 17:36:58 +1200

    Created and finished with all the project files

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a memory game project inspired and taught by the very talented :<a href="https://github.com/kubowania" target="_blank" rel="noopener noreferrer">Ania Kubow</a>. I have learnt several javascript methods such as:
+
+  <ul>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute" target="_blank" rel="noopener noreferrer">setAttribute()</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute" target="_blank" rel="noopener noreferrer">getAttribute()</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/API/Element/removeAttribute" target="_blank" rel="noopener noreferrer">removeAttribute()</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push" target="_blank" rel="noopener noreferrer">push()</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement" target="_blank" rel="noopener noreferrer">createElement()</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild" target="_blank" rel="noopener noreferrer">appendChild()</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort" target="_blank" rel="noopener noreferrer">sort()</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration" target="_blank" rel="noopener noreferrer">For Loops</a></li>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random" target="_blank" rel="noopener noreferrer">Math.random()</a></li>
+  </ul>
+
+I have also tried to use <a href="https://javascript.info/arrow-functions-basics" target="_blank" rel="noopener noreferrer">arrow functions </a>. Special thanks to Ania
---
+++ js/app.js
@@ -0,0 +1,123 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // cards options
+ const cardsArray = [
+  {
+    name : 'fries',
+    img : '/images/fries.png'
+  },
+  {
+    name : 'fries',
+    img : '/images/fries.png'
+  },
+  {
+    name : 'cheeseburger',
+    img : '/images/cheeseburger.png'
+  },
+  {
+    name : 'cheeseburger',
+    img : '/images/cheeseburger.png'
+  },
+  {
+    name : 'hotdog',
+    img : '/images/hotdog.png'
+  },
+  {
+    name : 'hotdog',
+    img : '/images/hotdog.png'
+  },
+  {
+    name : 'ice-cream',
+    img : '/images/ice-cream.png'
+  },
+  {
+    name : 'ice-cream',
+    img : '/images/ice-cream.png'
+  },
+  {
+    name : 'milkshake',
+    img : '/images/milkshake.png'
+  },
+  {
+    name : 'milkshake',
+    img : '/images/milkshake.png'
+  },
+  {
+    name : 'pizza',
+    img : '/images/pizza.png'
+  },
+  {
+    name : 'pizza',
+    img : '/images/pizza.png'
+  }
+];
+const grid = document.querySelector('.grid');
+const results = document.querySelector('#results');
+const header = document.querySelector('h3');
+
+
+let cardsChosenId = [];
+let cardsWon = [];
+let cardsChosen = [];
+
+
+//create board
+
+const createBoard = () => {
+  for(let i = 0; i < cardsArray.length; i++){
+    let card = document.createElement ('img');
+    card.setAttribute('src', '/images/blank.png');
+    card.setAttribute('data-id', i );
+    card.addEventListener('click', flipCard);
+    grid.appendChild(card);
+
+  }
+}
+
+
+// === check for matches
+const  checkForMatch = () => {
+  let cards = document.querySelectorAll('img');
+  const optionOneId = cardsChosenId[0];
+  const optionTwoId = cardsChosenId[1];
+
+  if (cardsChosen[0] === cardsChosen[1]) {
+    alert('Yeeeh!! You found a match');
+    cards[optionOneId].setAttribute('src', '/images/white.png');
+    cards[optionTwoId].setAttribute('src', '/images/white.png');
+    cardsWon.push(cardsChosen);
+  }else{
+    cards[optionOneId].setAttribute('src', '/images/blank.png');
+    cards[optionTwoId].setAttribute('src', '/images/blank.png');
+    alert(' Oops! Sorry, try again !!')
+  }
+  cardsChosen = [];
+  cardsChosenId = [];
+  results.innerHTML = cardsWon.length;
+
+  if(cardsWon.length === cardsArray.length/2) {
+    results.innerHTML = 'Congratulations!! You have found them all!! '
+    header.innerHTML = results.innerHTML;
+
+ }
+}
+
+
+  // === Flipping the card
+
+  function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardsArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardsArray[cardId].img);
+    if(cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500)
+    }
+
+  };
+
+  cardsArray.sort(() => 0.5 - Math.random());
+
+  createBoard();
+
+})
